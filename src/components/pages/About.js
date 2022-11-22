dc460889557ac050c19fd6f93cc491b10829f126
chrisponz11 <chrisponz11@gmail.com>
2022-11-22 09:44:02 -0500

    attempt to add profile pic

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../../assets/styles/About.css';
-import '../../assets/img/2020profilepic.jpg';
 
 export default function About() {
   return (
@@ -19,9 +18,6 @@ export default function About() {
                 and React to build exceptional customer experiences. Highly adept at working independently and in a collaborative work environment 
                 - finding solutions and determining customer satisfaction. Passionate about using programming to deliver an immersive and engaging 
                 user experience through efficient web development, proactive feature optimization, and relentless debugging!
-      </p>
-      <p id = "secondP">
-      
       </p>
     </div>
   );
